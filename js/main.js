74e2c25ec65f91a945fc932ccd135973b23d1cf1
ehsan <ebaharinasab.eb@gmail.com>
2021-06-09 08:49:33 +0430

    create navbar and header

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let isLargeWindow;
+window.onresize = () => {
+	isLargeWindow = window.innerWidth > 991;
+};
+
+window.onscroll = () => {
+	if (isLargeWindow) {
+		navbarShrink();
+	} else {
+		removeShrink();
+	}
+};
+
+
+function navbarShrink() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		notInTop();
+	} else {
+		inTop();
+	}
+}
+
+function inTop() {
+	document.getElementById('my-navbar').classList.remove('navbar-shrink');
+	console.log('in top');
+}
+
+function notInTop() {
+	document.getElementById('my-navbar').classList.add('navbar-shrink');
+	console.log('not in top');
+}
+
+function removeShrink() {
+	document.getElementById('my-navbar').classList.remove('navbar-shrink');
+}
